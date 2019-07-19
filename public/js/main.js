f11cc79f57f839beb5adb065908a1b84f9e7bdce
Pierre Baize <pierre@openfin.co>
2019-07-19 14:34:51 -0400

    golden tab hack

--- public/js/main.js
+++ public/js/main.js
@@ -3,34 +3,17 @@
 
 var config = {
     content: [{
-        type: 'row',
-        content: [
-    	    {
-                type:'component',
-                componentName: 'example',
-                componentState: { text: 'Component 1' }
-    	    },
-            {
-                type:'component',
-                componentName: 'example',
-                componentState: { text: 'Component 2' }
-    	    },
-            {
-                type:'component',
-                componentName: 'browserView',
-                componentState: { text: 'Component 3' }
-    	    }
-  	    ]
+        type: 'stack',
+        content: []
     }]
 };
 
 if (typeof fin !== 'undefined') {
-    fin.desktop.main(() => init().catch(console.error));
-} else {
-    document.querySelector('#of-version').innerText =
-        'The fin API is not available - you are probably running in a browser.';
+    init()
 }
-
+var myLayout;
+const views = [];
+window.views = views;
 //once the DOM has loaded and the OpenFin API is ready
 async function init() {
 
@@ -61,7 +44,7 @@ async function init() {
         const res = await fetch('url.json');
         const body = await res.json();
         console.log(body);
-        if(body.url) url = body.url;
+        if (body.url) url = body.url;
     } catch (error) {
         console.error(error);
         url = 'https://bing.com';
@@ -75,98 +58,132 @@ async function init() {
     })
     // app.on('run-requested', () => location.reload());
 
-    //Only launch new windows from the main window.
-    console.log(win);
-    if (win.identity.name === 'OpenfinPOC') {
-        // subscribing to the run-requested events will allow us to react to secondary launches, clicking on the icon once the Application is running for example.
-        // for this app we will  launch a child window the first the user clicks on the desktop.
-        app.once('run-requested', async () => {
-            await fin.Window.create({
-                name: 'childWindow',
-                url: location.href,
-                defaultWidth: 320,
-                defaultHeight: 320,
-                autoShow: true
-            });
-        });
+    // const view = await fin.BrowserView.create({
+    //       uuid: win.identity.uuid,
+    //       name: 'view',
+    //       url:'https://www.duckduckgo.com',
+    //       backgroundColor: '#fff',
+    //       autoResize: { width: false, height: false },
+    //       bounds: {
+    //             x: 5,
+    //             y: 35,
+    //             width: size.width - 10,
+    //             height: size.height - 40
+    //       },
+    //       target: win.identity
+    // });
 
-        const view = await fin.BrowserView.create({
-            uuid: win.identity.uuid,
-            name: 'view',
-            url:'https://www.duckduckgo.com',
-            backgroundColor: '#fff',
-            autoResize: { width: false, height: false },
-            bounds: {
-                x: 5,
-                y: 35,
-                width: size.width - 10,
-                height: size.height - 40
-            },
-            target: win.identity
-        });
-
-        window.bv = view;
-
-        const ro = new ResizeObserver( entries => {
-            for (let entry of entries) {
-                const cr = entry.contentRect;
-                console.log('Element:', entry.target);
-                console.log(`Element size: ${cr.width}px x ${cr.height}px`);
-                console.log(`Element padding: ${cr.top}px ; ${cr.left}px`);
-
-                var rect = entry.target.getBoundingClientRect();
-                console.log(rect.top, rect.right, rect.bottom, rect.left);
-                // height
-                // width
-                // top
-                // left
-                // right
-                // bottom
-                view.setBounds({
-                    height: Math.floor(cr.height),
-                    width: Math.floor(cr.width),
-                    y: Math.floor(rect.top),
-                    x: Math.floor(rect.left),
-                    right: Math.floor(rect.right),
-                    bottom: Math.floor(rect.bottom)
-                }).catch(console.error).then(() => console.log('did it'));
-            }
-        });
-
-        var myLayout = new GoldenLayout( config );
-        const element = $(`<div id="bv-container"></div>`);
-        myLayout.registerComponent( 'example', function( container, state ){
-            container.getElement().html( '<h2>' + state.text + '</h2>');
-        });
-        myLayout.registerComponent( 'browserView', function( container, state ){
-            // Append it to the DOM
-            container.getElement().append( element );
-        });
-
-        myLayout.init();
-        const bvContainer = document.querySelector('#bv-container');
-            // Observe one or multiple elements
-
-            ro.observe(bvContainer);
-
-        const delay = await new Promise(r => setTimeout(() => {
-            r();
-        }, 500));
-        setInterval(async () => {
-            const { title } = await view.getInfo();
-            const titleSpan = document.querySelector('.openfin-top-bar-title');
-            titleSpan.innerHTML = title;
-        }, 500);
-    }
+    // window.bv = view;
 
 
+
+    myLayout = new GoldenLayout(config);
+    window.myLayout = myLayout
+
+    myLayout.registerComponent('browserView', function (container, state) {
+        const elementId = `bv-container-${state.name}`
+        const element = $(`<div class="bv-container" id="${elementId}"></div>`);
+        // Append it to the DOM
+        container.getElement().append(element);
+    });
+    myLayout.init();
+
+    // const obs = addBrowserView(view)
+
+    // obs()
+    // const view2 = await fin.BrowserView.create({
+    //     uuid: win.identity.uuid,
+    //     name: 'view2',
+    //     url: 'https://www.bing.com',
+    //     backgroundColor: '#fff',
+    //     autoResize: { width: false, height: false },
+    //     bounds: {
+    //         x: 5,
+    //         y: 35,
+    //         width: size.width - 10,
+    //         height: size.height - 40
+    //     },
+    //     target: win.identity
+    // });
+    // const obs2 = addBrowserView(view2);
+    // obs2()
+
+
+
+
+    await fin.InterApplicationBus.subscribe({ uuid: '*' }, 'tab-added', async ({ ids, active }) => {
+        // console.log(ids)
+        const newViews = ids.map(id => fin.BrowserView.wrapSync(id));
+        views.push(...newViews)
+        // console.log(newViews)
+        // const oldCount = views.length;
+        // window.views.push(...newViews);
+        // activeView = oldCount + active;
+        newViews.map(addBrowserView).map(f => f())
+    })
+    fin.InterApplicationBus.send({uuid: win.identity.uuid, name: win.identity.uuid}, 'up') 
+    await fin.InterApplicationBus.subscribe({ uuid: '*' }, 'should-tab-to', async (id) => {
+        fin.InterApplicationBus.send(id, 'tab-added', { ids: views.map(v => v.identity) });
+        // let curView;
+        Promise.all(views.map(async (view, i) => {
+            // if (i === activeView) {
+            //     curView = view;
+            //     await view.hide();
+            // }
+            await view.attach(id);
+        }))
+        // await curView.hide();
+        // await curView.show();
+        await win.close();
+    })
 }
+function addBrowserView(view) {
+    // setInterval(async () => {
+    //     const { title } = await view.getInfo();
+    //     const titleSpan = document.querySelector('.openfin-top-bar-title');
+    //     titleSpan.innerHTML = title;
+    // }, 500);
+    const elementId = `bv-container-${view.identity.name}`
+    var newItemConfig = {
+        type: 'component',
+        componentName: 'browserView',
+        componentState: { name: view.identity.name }
+    };
+    myLayout.root.contentItems[0].addChild(newItemConfig)
+    const ro = new ResizeObserver(entries => {
+        for (let entry of entries) {
+            const cr = entry.contentRect;
+            console.log('Element:', entry.target);
+            console.log(`Element size: ${cr.width}px x ${cr.height}px`);
+            console.log(`Element padding: ${cr.top}px ; ${cr.left}px`);
 
+            var rect = entry.target.getBoundingClientRect();
+            console.log(rect.top, rect.right, rect.bottom, rect.left);
+            // height
+            // width
+            // top
+            // left
+            // right
+            // bottom
+            view.setBounds({
+                height: Math.floor(cr.height),
+                width: Math.floor(cr.width),
+                y: Math.floor(rect.top),
+                x: Math.floor(rect.left),
+                right: Math.floor(rect.right),
+                bottom: Math.floor(rect.bottom)
+            }).catch(console.error).then(() => console.log('did it'));
+        }
+    });
+    return () => {
+        const bvContainer = document.getElementById(elementId);
+        ro.observe(bvContainer);
+    }
+}
 var mainWindow = fin.desktop.Window.getCurrent();
 
 // Add css and HTML for the top bar.
 
 let backgroundColor = "#1C1C31";
 let textColor = "#FFFFFF";
-var topBarHTML = `
-`;
+
